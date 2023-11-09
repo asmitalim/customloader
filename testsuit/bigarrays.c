441884f812422a3acb164b4dfcbf627dd578f462
“asmital” <limaye.asmita@gmail.com>
2023-11-09 11:16:39 +0530

    updated with instrumentation and bigarray taking different chunksize

--- testsuit/bigarrays.c
+++ testsuit/bigarrays.c
@@ -4,22 +4,63 @@
 #include <fcntl.h>
 #include <assert.h>
 
-#define  JUMBOSIZE 4096*1000
+#include "instruments.h"
+#define  MAXPAGES  2048
+
+#define  JUMBOSIZE 4096*MAXPAGES
 
 unsigned int array1[JUMBOSIZE];
 unsigned int array2[JUMBOSIZE] ;
 
-int N = 500 ; 
+#define  MINCHUNKSIZE 5
 
+int N = MINCHUNKSIZE ; 
+
+// more for the pattern
 unsigned int NUMBER1 = 29 ;
 unsigned int NUMBER2 = 21;
 unsigned int NUMBER3 = 10567 ;
 
 int main(int argc, char **argv) {
+
+	if ( argc < 2 ) {
+		fprintf(stderr,"Usage bigarray <chunks min %d, max 1000>\n",MINCHUNKSIZE);
+		exit(1);
+	}
+
+	int pages = MAXPAGES ;
+	int chunks = N  ; 
+	int ret = 0 ;
+	/*
+	int ret = sscanf(argv[1],"%d",&pages);
+	if( ret != 1 ) {
+		fprintf(stderr, "pages should be integer\n");
+		fprintf(stderr,"Usage bigarray <pages>  <chunks>\n");
+		exit(1);
+	}
+	*/
+	ret = sscanf(argv[1],"%d",&chunks);
+	if( ret != 1)  {
+		fprintf(stderr, "chunks should be integer\n");
+		fprintf(stderr,"Usage bigarray <pages>  <chunks>\n");
+		exit(1);
+	}
+
+	if ( chunks < MINCHUNKSIZE || chunks > 1000 ) {
+		fprintf(stderr,"chunk size reset to %d\n",MINCHUNKSIZE);
+		N = MINCHUNKSIZE ;
+	}
+
+	N = chunks ;
+	printf("pages %d chunks %d\n", pages, N);
+
+
+
+
+
 	FILE *fp ; 
 	char buffer[4000] ; 
 
-	assert( N > 6);
 
 
 // Write some memory
@@ -96,6 +137,7 @@ int main(int argc, char **argv) {
 				fputs("\n",fp);
 
 				fclose(fp);
+				instrumentstats("Big Array Execution");
 				exit(0);
 
 }
---
+++ testsuit/instruments.h
@@ -0,0 +1,16 @@
+#ifndef INSTRUMENTS_H
+#define INSTRUMENTS_H
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/resource.h>
+#include <ctype.h>
+#include <stdint.h>
+
+
+void instrumentstats(char *prefixstring) ;
+
+#endif
